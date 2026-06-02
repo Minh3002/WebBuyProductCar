7586b2f4b6661ea71bd7d21a82d692c84a4822c6
Minh <truonggiaminh123@gmail.com>
2026-06-02 21:37:39 +0700

    commit

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:3000/api/v1',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
+  withCredentials: true,
 });
 
 // Add a request interceptor
