94bf0be12dd68ab86d0a5f9944b38c67977c897f
Minh <truonggiaminh123@gmail.com>
2026-06-03 18:19:13 +0700

    commit fixx hiện dữ liệu ở veercel và localhost

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -4,8 +4,7 @@ const axiosClient = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api/v1',
   headers: {
     'Content-Type': 'application/json',
-  },
-  withCredentials: true,
+  }
 });
 
 // Add a request interceptor
